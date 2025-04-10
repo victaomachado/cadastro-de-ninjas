2ef8c897eae9b1ffccf435df6f68ce77cf8958a3
Victor Hugo <161888868+victaomachado@users.noreply.github.com>
2025-04-10 10:01:44 -0300

    first commit

---
+++ src/main/java/br/com/victorhm/cadastro_de_ninjas/CadastroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package br.com.victorhm.cadastro_de_ninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+	}
+
+}
